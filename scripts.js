5443c9564fab526dc687e36f70c9952705c0a2cd
Tushabe barbra <Gumbouwe>
2018-12-17 17:40:08 +0200

    add links

--- scripts.js
+++ scripts.js
@@ -4,14 +4,17 @@ var side2= parseFloat(document.getElementById("side2").value);
 var side3= parseFloat(document.getElementById("side3").value);
 if(side1 === side2&& side2 === side3){
 alert("Equilateral");
+window.location.href="Equilateral.html";
 }
 else if(side1 === side2 || side1 === side3 || side2 === side3){
 alert("Isosceles");
+window.location.href="isosceles.html";
 }
 else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
 alert("Not a definite triangle");
 }
 else{
 alert("Scalene");
+window.location.href="scalene.html";
 }
 }
